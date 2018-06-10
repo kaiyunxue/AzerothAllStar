246a65a469881feef3ba102d96c0ed6f59f571dc
KevinXue <kevinxue0239@gmail.com>
2018-06-10 18:58:41 -0400

    coding on main menu page

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject backGround;
+    public MenuBar bar;
+}
---
+++ Assets/Scripts/UI/MenuBar.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuBar : MonoBehaviour
+{
+    Graphic[] graphics;
+    float a;
+    Coroutine currentCoroutine;
+    private void Awake()
+    {
+        graphics = GetComponentsInChildren<Graphic>();
+    }
+    public void ShowUI()
+    {
+        if (currentCoroutine != null)
+            StopCoroutine(currentCoroutine);
+        currentCoroutine = StartCoroutine(showUI());
+    }
+    public void HideUI()
+    {
+        if (currentCoroutine != null)
+            StopCoroutine(currentCoroutine);
+        currentCoroutine = StartCoroutine(hideUI());
+    }
+    IEnumerator showUI()
+    {
+        if(a <= 1)
+        {
+            a = 1;
+            currentCoroutine = null;
+            yield return null;
+        }
+        else
+        {
+            a += 0.01f;
+            foreach (var v in graphics)
+            {
+                Color c = v.color;
+                c.a = a;
+                v.color = c;
+            }
+            yield return new WaitForEndOfFrame();
+            currentCoroutine = StartCoroutine(showUI());
+        }
+    }
+    IEnumerator hideUI()
+    {
+        if (a >= 0)
+        {
+            a = 0;
+            currentCoroutine = null;
+            yield return null;
+        }
+        else
+        {
+            a += 0.01f;
+            foreach (var v in graphics)
+            {
+                Color c = v.color;
+                c.a = a;
+                v.color = c;
+            }
+            yield return new WaitForEndOfFrame();
+            currentCoroutine = StartCoroutine(hideUI());
+        }
+    }
+}
