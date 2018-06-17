0a6c336ea0f8eadb3f95730643db66723637243f
KevinXue <kevinxue0239@gmail.com>
2018-06-17 00:03:53 -0400

    Build the heroes and skills packet.

---
+++ Assets/Scripts/HeroPacket.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class HeroPacket : ScriptableObject {
+
+    public int id;
+
+    public HeroIcon icon;
+    public Hero heroPrefab;
+    public string heroName; //has repetition
+    public string race;
+    public string energy;
+    public int attack;
+    public int spell;
+    public int summon;
+    public int defence;
+    public string instruction;
+    public SkillInstruction[] instructions;
+}
---
+++ Assets/Scripts/UI/SkillsInstruction.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class SkillInstruction : ScriptableObject
+{
+    public Sprite skillIcon;
+    public string nameAndFormula;
+    public float cd;
+    public float mana;
+    public string instruction;
+}
