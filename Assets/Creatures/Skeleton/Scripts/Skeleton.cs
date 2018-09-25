00df580df76499199f662004f90af418fae89912
KevinXue <kevinxue0239@gmail.com>
2018-09-24 23:25:41 -0400

    Summon Skeleton

--- Assets/Creatures/Skeleton/Scripts/Skeleton.cs
+++ Assets/Creatures/Skeleton/Scripts/Skeleton.cs
@@ -1,15 +1,17 @@
-﻿using System;
+﻿//Programer: KevinX
+//code date:9/24/2018
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 
-public class #SCRIPTNAME# : CreatureBehavuourController
+public class Skeleton : CreatureBehavuourController
 {
     protected override void Awake()
     {
         base.Awake();
-		hatredCurve = ConstHatredCurve.instance.//GetCurve();
+        hatredCurve = ConstHatredCurve.instance.GetMobCurve();
     }
     protected override void OnEnable()
     {
@@ -23,37 +25,31 @@ public class #SCRIPTNAME# : CreatureBehavuourController
     }
 	IEnumerator startBehave()
     {
-        yield return new WaitForSeconds(4f); //the time the mob will wait for the birth animation;
-		//do something
-
+        yield return new WaitForSeconds(0.2f); //the time the mob will wait for the birth animation;
         StartCoroutine(behaveUpdate()); //update
     }
     IEnumerator behaveUpdate()
     {
         yield return new WaitForEndOfFrame();
-		//do something
-		
-
-		//Only a reference
-
-		//Ray ray = new Ray(transform.position, transform.forward);
-        //bool isNearTarget = false; ;
-        //foreach(var hit in Physics.RaycastAll(ray, 1))
-        //{
-        //    if(hit.collider.gameObject == target)
-        //    {
-        //        GetComponent<Animator>().SetBool("Attack", true);
-        //        isNearTarget = true;
-        //        break;
-        //    }
-        //}
-        //if(!isNearTarget)
-        //{
-        //    GetComponent<Animator>().SetBool("Attack", false);
-        //    Vector3 dir = target.transform.position - transform.position;
-        //    dir.y = 0;
-        //    transform.position += dir.normalized * Time.deltaTime;
-        //}
+        //do something
+        Ray ray = new Ray(transform.position, transform.forward);
+        bool isNearTarget = false; ;
+        foreach (var hit in Physics.RaycastAll(ray, 1))
+        {
+            if (hit.collider.gameObject == target)
+            {
+                GetComponent<Animator>().SetBool("Attack", true);
+                isNearTarget = true;
+                break;
+            }
+        }
+        if (!isNearTarget)
+        {
+            GetComponent<Animator>().SetBool("Attack", false);
+            Vector3 dir = target.transform.position - transform.position;
+            dir.y = 0;
+            transform.position += dir.normalized * Time.deltaTime * 0.4f;
+        }
         StartCoroutine(behaveUpdate());
     }
 
---
+++ Assets/Effects/Scripts/Arthas/SummonField.cs
@@ -0,0 +1,36 @@
+﻿//Programer: KevinX
+//code date:9/24/2018
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SummonField : SkillItemsBehaviourController
+{
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+    }
+    public override void SetTarget(GameObject target)
+    {
+        base.SetTarget(target);
+    }
+    protected override IEnumerator DestorySelf()
+    {
+        return base.DestorySelf();
+    }
+    public override IEnumerator Live()
+    {
+        return base.Live();
+    }
+
+    public override int GetMaxInstance()
+    {
+        return base.GetMaxInstance();
+    }
+}
