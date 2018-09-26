fa48df0406763e20746a2b451e0b1d82ff509150
KevinXue <kevinxue0239@gmail.com>
2018-09-26 15:05:59 -0400

    Varkier

---
+++ Assets/Creatures/Varkier/Varkier.cs
@@ -0,0 +1,105 @@
+﻿//Programer: KevinX
+//code date:9/26/2018
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Varkier : CreatureBehavuourController
+{
+    public float spellDis;
+    public float reviveDis;
+    public float speed;
+
+    [SerializeField]
+    Transform spellHand;
+
+    [SerializeField]
+    EffectBullet_0_Varkier bullet;
+    EffectBullet_0_Varkier bulletInstance;
+
+    bool hasBullet = false;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        hatredCurve = ConstHatredCurve.instance.GetMobCurve();
+    }
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+    }
+	private void Start()
+    {
+        SetTarget(getMaxHatredObject().gameObject);
+        StartCoroutine(startBehave());
+        StartCoroutine(switchTarget());
+    }
+	IEnumerator startBehave()
+    {
+        yield return new WaitForSeconds(0.1f); //the time the mob will wait for the birth animation;
+		//do something
+
+        StartCoroutine(behaveUpdate()); //update
+    }
+    IEnumerator behaveUpdate()
+    {
+        //do something
+
+
+        //Only a reference
+        if (Vector3.Distance(transform.position, target.transform.position) <= spellDis)
+        {
+            GetComponent<Animator>().SetBool("Spell", true);
+            if(!hasBullet)
+            {
+                hasBullet = true;
+                bulletInstance = KOFItem.InstantiateByPool(bullet, GameController.instance.transform, gameObject.layer);
+                bulletInstance.Follow(this.spellHand);
+                yield return new WaitForSeconds(1.35f);
+                bulletInstance.Shoot(this.target);
+                yield return new WaitForSeconds(1.04f);
+                hasBullet = false;
+            }
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("Spell", false);
+            Vector3 dir = target.transform.position - transform.position;
+            transform.position += dir.normalized * Time.deltaTime * speed;
+        }
+        yield return new WaitForEndOfFrame();
+        StartCoroutine(behaveUpdate());
+    }
+
+
+
+
+	//OVERRIDE FUNCTIONS
+    public override IEnumerator Die()
+    {
+        return base.Die();
+    }
+    protected override IEnumerator Live()
+    {
+        return base.Live();
+    }
+    public override void SetTarget(GameObject target)
+    {
+        base.SetTarget(target);
+        transform.LookAt(target.transform.position);
+    }
+    protected override KOFItem getMaxHatredObject()
+    {
+        return base.getMaxHatredObject();
+    }
+    public override int GetMaxInstance()
+    {
+        return base.GetMaxInstance();
+    }
+	protected override IEnumerator switchTarget()
+    {
+        return base.switchTarget();
+    }
+}
---
+++ Assets/Effects/Scripts/Public/EffectBullet_0_Varkier.cs
@@ -0,0 +1,66 @@
+﻿//Programer: KevinX
+//code date:9/26/2018
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EffectBullet_0_Varkier : SkillItemsBehaviourController
+{
+    public float speed;
+    [SerializeField]
+    GameObject trials;
+    Coroutine followCoroutine;
+    Coroutine shootCoroutine;
+    public void Follow(Transform aim)
+    {
+        followCoroutine = StartCoroutine(follow(aim));
+        trials.SetActive(false);
+    }
+    IEnumerator follow(Transform aim)
+    {
+        transform.position = aim.position;
+        yield return new WaitForEndOfFrame();
+        followCoroutine = StartCoroutine(follow(aim));
+    }
+    public void Shoot(GameObject target)
+    {
+        this.target = target;
+        trials.SetActive(true);
+        StopCoroutine(followCoroutine);
+        shootCoroutine = StartCoroutine(shoot());
+    }
+    public IEnumerator shoot()
+    {
+        Vector3 dir = target.transform.position - transform.position;
+        transform.position += dir.normalized * speed * Time.deltaTime;
+        yield return new WaitForEndOfFrame();
+        shootCoroutine = StartCoroutine(shoot());
+    }
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+    }
+    public override void SetTarget(GameObject target)
+    {
+        base.SetTarget(target);
+    }
+    protected override IEnumerator DestorySelf()
+    {
+        return base.DestorySelf();
+    }
+    public override IEnumerator Live()
+    {
+        return base.Live();
+    }
+
+    public override int GetMaxInstance()
+    {
+        return base.GetMaxInstance();
+    }
+}
